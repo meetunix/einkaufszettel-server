a4f47f91a429a5ca0f6f6b1b195b1af7604ca3b8
Martin Steinbach <m.steinbach@posteo.de>
2023-06-07 22:21:34 +0200

    simple example upload

--- test-tools/00-insert-example.sh
+++ test-tools/00-insert-example.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+HOST="$1"
+
+if [[ -z $HOST ]] ; then
+  HOST="http://127.0.0.1:18080"
+fi
 
 FILE="./example.json"
-URL="$1/r0/ez/76f2c9e4-ea57-4df6-bdbf-cc7a5301df80"
+URL="$HOST/r0/ez/76f2c9e4-ea57-4df6-bdbf-cc7a5301df80"
 
 curl -v -H "Content-Type: application/json" -XPUT -s $URL --data @$FILE
 
 curl -v -H "Accept: application/json" -XGET -s $URL
+echo
